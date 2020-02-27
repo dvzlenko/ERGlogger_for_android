2ff1eea4450f83306c380f840bcf32d07ca6ac23
zoidberg <dvzlenko@gmail.com>
2020-02-27 10:43:39 +0300

    Permission requests were implementated here and the first-start hanging out bug was fixed

--- app/src/main/java/com/ERG/erglogger/MainActivity.java
+++ app/src/main/java/com/ERG/erglogger/MainActivity.java
@@ -1,121 +1,126 @@
 package com.ERG.erglogger;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.hardware.usb.UsbDevice;
 import android.hardware.usb.UsbDeviceConnection;
 import android.hardware.usb.UsbManager;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.util.Log;
 
 import com.hoho.android.usbserial.driver.UsbSerialDriver;
 import com.hoho.android.usbserial.driver.UsbSerialPort;
 
+import java.security.Permission;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    UsbManager manager;
+    UsbSerialDriver driver;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // detach intent
-        /*final Activity obj = this;
-        BroadcastReceiver detachReceiver = new BroadcastReceiver() {
-            public void onReceive(Context context, Intent intent) {
-                if(intent.getAction().equals(UsbManager.ACTION_USB_DEVICE_DETACHED))
-                    Global.EXTRA_Message = "ERG logger was detached!\nSorry, the application must be closed!!!";
-                    Global.RiseError(obj, true);
+        //Getting the storage permission status
+        int result = ContextCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if (result == PackageManager.PERMISSION_GRANTED)
+            Log.i("storage permission", "GRANTED");
+        else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+            Log.i("storage permission", "DENIED");
+            return;
+        }
+        // creating a directory for files storing
+        if (!Global.directory.exists()) {
+            boolean success = Global.directory.mkdirs();
+            if (!success) {
+                Global.EXTRA_Message = "The directory for file storage was not created :(\n\n" +
+                        "The ERGlogger application requires the permission to access the storage of the device!";
+                Global.RiseError(this, false);
             }
-        };
-        IntentFilter filter = new IntentFilter();
-        filter.addAction(UsbManager.ACTION_USB_DEVICE_DETACHED);
-        registerReceiver(detachReceiver, filter);*/
+            Log.i("directory", "CREATED");
+        }
+        else
+            Log.i("directory", "EXISTS");
 
-        // find the driver for the device 0483:5740 or 1155:22336 described in CustomProber class
-        UsbManager manager = (UsbManager) getSystemService(Context.USB_SERVICE);
+    }
+
+    // do all the job
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // usb initial stuff and permissions
+        manager = (UsbManager) getSystemService(Context.USB_SERVICE);
         if (manager.getDeviceList().isEmpty()) {
-            /*Global.devinfo = (
-                    "hello\r\n" +
-                    "167321907\r\n" +
-                    "ERG-T-07\r\n" +
-                    "6731\r\n" +
-                    "STM32F103CBT6 (72 MHz, 20 kB)\r\n" +
-                    "AD7799 (24 bit, 3 channel)\r\n" +
-                    "MX25L12835VF (16 MB, FLASH-NOR)\r\n" +
-                    "700-102BAA-B00 (0.06%, class A)\r\n" +
-                    "                  0.003017108615\r\n" +
-                    "                 -0.000662285424\r\n" +
-                    "                  0.000640361011\r\n" +
-                    "                999.877900207255\r\n" +
-                    "                  0.002442093427\r\n" +
-                    "                 -0.000881547672\r\n" +
-                    "                 -0.000398092095\r\n" +
-                    "               1000.529654264777\r\n" +
-                    "                  0.051532188710\r\n" +
-                    "                  0.597681144814\r\n" +
-                    "                 -0.287017806897\r\n" +
-                    "                999.612500351621\r\n").split("\r\n");
-            Intent intent = new Intent(this, TActivity.class);
-            startActivity(intent);*/
             Global.EXTRA_Message = "Cannot find any USB device";
             Global.RiseError(this, true);
             finish();
-        } else {
+        }
+        else {
             List<UsbSerialDriver> availableDrivers = CustomProber.getCustomProber().findAllDrivers(manager);
             if (availableDrivers.isEmpty()) {
-                Global.EXTRA_Message = "The device connected to the smartphone is unsupported:\n\nDevice 0483:5740 was not found";
+                Global.EXTRA_Message = "The device connected to the SmartPhone is unsupported:\n\nDevice 0483:5740 was not found";
                 Global.RiseError(this, true);
-                finish();
-            }
-            else {
-                // open connection
-                UsbSerialDriver driver = availableDrivers.get(0);
-                UsbDeviceConnection connection = manager.openDevice(driver.getDevice());
-                if (connection == null) {
-                    //manager.requestPermission(driver.getDevice(), this);
-                    Global.EXTRA_Message = "Failed to connect the logger\n\nProbably, you need to add the appropriate permission?";
-                    Global.RiseError(this, true);
-                    finish();
+            } else {
+                // check the permission and explicitly ask for it
+                // TODO: Fix the  twice-asking trouble!!!
+                driver = availableDrivers.get(0);
+                if (!manager.hasPermission(driver.getDevice())) {
+                    Log.i("usb permission", "DENIED");
+                    manager.requestPermission(
+                            driver.getDevice(),
+                            PendingIntent.getBroadcast(this, 0, new Intent("com.android.example.USB_PERMISSION"), PendingIntent.FLAG_ONE_SHOT));
+                    return;
+                } else {
+                    Log.i("usb permission", "GRANTED");
+                    goOn();
                 }
-                Global.port = driver.getPorts().get(0); // Most devices have just one port (port 0)
-                try {
-                    Global.port.open(connection);
-                } catch (Exception exp) {
-                    Global.EXTRA_Message = "Port open failed.\n\nException message is:\n\n" + exp.getMessage();
-                    Global.RiseError(this, true);
-                    finish();
-                }
-                try {
-                    Global.port.setParameters(115200, 8, UsbSerialPort.STOPBITS_1, UsbSerialPort.PARITY_NONE);
-                } catch (Exception exp) {
-                    Global.EXTRA_Message = "Port adjustment failed.\n\nException messagew is:\n\n" + exp.getMessage();
-                    Global.RiseError(this, true);
-                    finish();
-                }
-                // initial hand-shake
-                Global.CDC_Send("hello\r\n", this);
-                String reply = Global.CDC_Get_String(this);
-                Global.devinfo = reply.split("\r\n");
             }
         }
     }
 
-    // start T/PT Activity
-    @Override
-    protected void onStart() {
-        super.onStart();
-        startActivity();
-    }
-
-    // quit the main activity
+    // quit main activity after device was connected and all of the permissions were granted
     @Override
     protected void onStop() {
         super.onStop();
         finish();
     }
 
-    private void startActivity() {
+    private void goOn() {
+        UsbDeviceConnection connection = manager.openDevice(driver.getDevice());
+        Global.port = driver.getPorts().get(0);
+        try {
+            Global.port.open(connection);
+        } catch (Exception exp) {
+            exp.printStackTrace();
+            Log.i("usb permission", "Port Open Failed!");
+            Global.EXTRA_Message = "Port open failed.\n\nException message is:\n\n" + exp.getMessage();
+            Global.RiseError(this, true);
+        }
+        try {
+            Global.port.setParameters(115200, 8, UsbSerialPort.STOPBITS_1, UsbSerialPort.PARITY_NONE);
+        } catch (Exception exp) {
+            exp.printStackTrace();
+            Global.EXTRA_Message = "Port adjustment failed.\n\nException messagew is:\n\n" + exp.getMessage();
+            Global.RiseError(this, true);
+        }
+        // initial hand-shake
+        Global.CDC_Send("hello\r\n", this);
+        String reply = Global.CDC_Get_String(this);
+        Global.devinfo = reply.split("\r\n");
+        // TODO: make an additional check!!!
         if (Integer.valueOf(Global.devinfo[1]) == 167321907) {
             if (Global.devinfo[2].contains("ERG-T-")) {
                 Intent intent = new Intent(this, TActivity.class);
@@ -123,8 +128,37 @@ public class MainActivity extends AppCompatActivity {
             } else if (Global.devinfo[2].contains("ERG-TP-")) {
                 Intent intent = new Intent(this, TPActivity.class);
                 startActivity(intent);
+            } else {
+                Global.EXTRA_Message = "Device was not recognized :(";
+                Global.RiseError(this, true);
             }
         }
+    }
+
+
+
+    public void getStorageAccess() {
+        //Getting the permission status
+        int result = ContextCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if (result == PackageManager.PERMISSION_GRANTED)
+            Log.i("storage permission", "GRANTED");
+        else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+            Log.i("storage permission", "DENIED");
+            return;
+        }
+        // creating a directory for files storing
+        if (!Global.directory.exists()) {
+            boolean success = Global.directory.mkdirs();
+            if (!success) {
+                Global.EXTRA_Message = "The directory for file storage was not created :(\n\n" +
+                        "The ERGlogger application requires the permission to access the storage of the device!";
+                Global.RiseError(this, false);
+            }
+            Log.i("directory", "CREATED");
+        }
+        else
+            Log.i("directory", "EXISTS");
     }
 }
 
